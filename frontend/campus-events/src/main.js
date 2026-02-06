e042ab832c6b2da04bdda19c3879b71c91e10471
Vidocq17 <AvidanB1704@gmail.com>
2026-02-06 16:06:12 +0100

    chore(develop): merge backend and frontend configuration

--- frontend/campus-events/src/main.js
+++ frontend/campus-events/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+    .use(router)
+    .mount('#app')
